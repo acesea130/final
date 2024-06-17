0389c882b54853405ebeaad27672c566ce260365
acesea130 <AceSea130@gmail.com>
2024-06-17 15:39:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
                             "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
                             "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
                             "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-                            "011651547,James,Jarzembowski,jjarzem@wgu.edu,25,12,22,33,SOFTWARE" };
+                            "A5,James,Jarzembowski,jjarzem@wgu.edu,25,12,22,33,SOFTWARE" };
 
     const int len = 5;
 
@@ -61,7 +61,7 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         classRoster->printAverageDaysInCourse(classRoster->v_classRosterArray[i]->getStudentID());
     }
-    
+
     classRoster->printByDegreeProgram(SOFTWARE);
     classRoster->remove("A3");
     classRoster->printAll();
